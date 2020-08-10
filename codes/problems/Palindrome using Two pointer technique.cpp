ae7f1ea90f4c4fd704b911790960ced2ef09af36
47karimasif <karimasif98@gmail.com>
2020-08-10 17:39:38 +0530

    updated

--- codes/problems/Palindrome using Two pointer technique.cpp
+++ codes/problems/Palindrome using Two pointer technique.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
-#include <vector>
 using namespace std;
 
 //palindrome using two pointer technique
 
 int main() {
-	string s;
-	cin>>s;
-	int i=0;
-	int l=s.length()-1;
-	while(i++<l--){
-	    if(s[i]!=s[l]){
-	        cout<<"Not a palindrome";
-	        return 0;
-	    }
-	}
-	cout<<"It's a palindrome";
+    string s;
+    getline(cin,s);
+    int i=0;
+    int l = s.length()-1;
+    while(i<l){
+        if(s[i++]!=s[l--]){
+            cout<<"not a palindrome";
+            return 0;
+        }
+    }
+    cout<<"its a palindrome";
+    
 	return 0;
 }
